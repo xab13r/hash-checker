5fe2ce418348dcf5ff6079eae6f97de66f9ffa3a
Lorenzo <xabier.nk@me.com>
2016-06-30 14:25:08 +0100

    Added the ability to pass a hash and check for its authenticity

--- hash_file_checker.py
+++ hash_file_checker.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+# python hash_file_checker.py -i input_file -o output.txt
 # -*- coding: utf-8 -*-
 
 import hashlib
 import argparse
+import sys
 
 
 def hashfile(input_file):
@@ -30,17 +31,71 @@ def hashfile(input_file):
                 sha256.update(data)
 
     except IOError:
-        print 'PLease check that the input file is valid'
+        print('Please check that the input file is valid\n')
 
-    return md5.hexdigest(), sha1.hexdigest(), sha256.hexdigest()
+    md5_digest = md5.hexdigest().upper()
+    sha1_digest = sha1.hexdigest().upper()
+    sha256_digest = sha256.hexdigest().upper()
+
+    return md5_digest, sha1_digest, sha256_digest
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument(dest='input_file', help='mandatory input file')
-    md5_digest, sha1_digest, sha256_digest = hashfile(
-        parser.parse_args().input_file)
 
-    print 'MD5:\t' + md5_digest.upper()
-    print 'SHA1:\t' + sha1_digest.upper()
-    print 'SHA256:\t' + sha256_digest.upper()
+    # Parse arguments from command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', dest='input_file', help='mandatory input file')
+    parser.add_argument('-o', dest='output_file', help='optional output file')
+    parser.add_argument('-t', dest='target', help='optional provided hash')
+
+    args = parser.parse_args()
+
+    # Check validity of input file
+    if args.input_file is None:
+        sys.exit("\nPlease specify the input file with -i [input_file]\n")
+
+    filein = args.input_file
+    fileout = args.output_file
+    target = args.target
+
+    # Compute hash
+    md5_digest, sha1_digest, sha256_digest = hashfile(filein)
+
+    # If hash has been provided, compare it to computed hash
+    if target is not None:
+        try:
+            target = target.upper()
+
+            if (target == sha1_digest):
+                print('\nFile:\t%s' % filein)
+                print('SHA1 Hash matched:')
+                print(sha1_digest)
+                print(target)
+                print('The file looks authentic.\n')
+
+            elif (target == md5_digest):
+                print('\nFile:\t%s' % filein)
+                print('MD5 Hash matched:')
+                print(md5_digest)
+                print(target)
+                print('The file looks authentic.\n')
+
+            elif (target == sha256_digest):
+                print('\nFile:\t%s' % filein)
+                print('SHA256 Hash matched:')
+                print(sha256_digest)
+                print(target)
+                print('The file looks authentic.\n')
+
+            else:
+                print('\nFile:\t%s' % filein)
+                print('WARNING: Try to redownload the file.\n')
+
+        except IOError:
+            print('Please check the hash you provided\n')
+
+    else:
+        print("\nFile:\t%s" % filein)
+        print('MD5:\t%s' % md5_digest)
+        print('SHA1:\t%s' % sha1_digest)
+        print('SHA256:\t%s\n' % sha256_digest)
